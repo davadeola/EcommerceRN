1baef6b8b6635a1663aaf7b8d8456ce30795930f
davadeola <adeoladavid15@gmail.com>
2022-05-19 10:55:06 +0300

    added animated dots on the walkthrough

--- screens/Walkthrough/Walkthrough.js
+++ screens/Walkthrough/Walkthrough.js
@@ -7,6 +7,58 @@ import {COLORS, SIZES, constants, FONTS} from '../../constants';
 const Walkthrough = () => {
   const scrollX = React.useRef(new Animated.Value(0)).current;
 
+  const Dots = () => {
+    //get current screen
+    const dotPosition = Animated.divide(scrollX, SIZES.width);
+
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {constants.walkthrough.map((item, index) => {
+          const dotColor = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [COLORS.dark08, COLORS.primary, COLORS.dark08],
+            extrapolate: 'clamp',
+          });
+
+          return (
+            <Animated.View
+              key={`dot-${index}`}
+              style={{
+                borderRadius: 5,
+                marginHorizontal: 6,
+                width: 10,
+                height: 10,
+                backgroundColor: dotColor,
+              }}
+            />
+          );
+        })}
+      </View>
+    );
+  };
+
+  const renderFooter = () => (
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: SIZES.height * 0.2,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: SIZES.padding,
+        paddingVertical: SIZES.height > 700 ? SIZES.padding : 20,
+      }}>
+      <Dots />
+    </View>
+  );
+
   return (
     <View style={{flex: 1, backgroundColor: COLORS.light}}>
       <Animated.FlatList
@@ -51,6 +103,7 @@ const Walkthrough = () => {
           </View>
         )}
       />
+      {renderFooter()}
     </View>
   );
 };
